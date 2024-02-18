7795312960ee059a8388aeb0abf425dc3c2e27ef
Melisa <andream_dearth@hotmail.com>
2024-02-18 17:36:39 +0000

    Adding structure folders with components, data JSON with projects, and project screenshots

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import dataProjects from './dataProjects.json';
 
 function App() {
   const [count, setCount] = useState(0)
