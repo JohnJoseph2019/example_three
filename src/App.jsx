8ebcae95649817fdf383310fc01e5c900a4139a6
Melisa <andream_dearth@hotmail.com>
2024-02-20 20:54:57 +0000

    Added the about page in navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MyNav from './components/Navbar'
-import { Contact, Custom404, Home, ProjectGallery } from './components/Pages'
+import { Contact, Custom404, Home, ProjectGallery, About } from './components/Pages'
 import './App.css'
 
 
@@ -12,6 +12,7 @@ function App() {
       <MyNav />
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
           <Route path='/portfolio' element={<ProjectGallery />} />
           <Route path='/contact' element={<Contact />} />
           <Route path='/404' element={<Custom404 />} />
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -12,9 +12,10 @@ function myNav() {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="./">Home</Nav.Link>
+            <Nav.Link href="./about">About</Nav.Link>
             <Nav.Link href="./portfolio">Portfolio</Nav.Link>
             <Nav.Link href="./contact">Contact</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+            {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
                 Another action
@@ -24,7 +25,7 @@ function myNav() {
               <NavDropdown.Item href="#action/3.4">
                 Separated link
               </NavDropdown.Item>
-            </NavDropdown>
+            </NavDropdown> */}
           </Nav>
         </Navbar.Collapse>
       </Container>
