5c87692efe2dc1ed1a1d4715d3721474270b4c40
Melisa <andream_dearth@hotmail.com>
2024-02-20 21:55:20 +0000

    Adding basic info to home, and about pages. Also photo of myself

--- src/components/Pages/About.jsx
+++ src/components/Pages/About.jsx
@@ -3,6 +3,39 @@ const About = () => {
     return (
     <>
         <h1>About me</h1>
+
+
+        <p>Hello, my name is Andrea. I'm currently studying front end web development and i'd love to switch career. It is challenching to start over again, but also very rewarding.</p>
+        <p>I am a mother of two wonderful children with 1 and 3 years and they are super cute and funny. They also teach me lot's of things and how to enjoy every single day.</p>
+
+        <section class="container-sm skillsSection" id="skills"><h2>Skills</h2>
+            <div class="container text-center mb-5">
+                <ul class="list-group list-group-flush border-secondary">
+                    <li class="list-group-item">Terminal  <i class="fa-solid bi-terminal"></i></li>
+                    <li class="list-group-item">Git  <i class="bi bi-git"></i></li>
+                    <li class="list-group-item">GitHub  <i class="bi bi-github"></i></li>
+                    <li class="list-group-item">HTML 5  <i class="bi bi-filetype-html"></i></li>
+                    <li class="list-group-item">CSS 3  <i class="bi bi-filetype-css"></i></li>
+                    <li class="list-group-item">Flexbox</li>
+                    <li class="list-group-item">Grid</li>
+                    <li class="list-group-item">Bootstrap  <i class="bi bi-bootstrap"></i></li>
+                    <li class="list-group-item toLearnSkills">JavaScript  <i class="bi bi-filetype-js"></i></li>
+                    <li class="list-group-item toLearnSkills">jQuery</li>
+                    <li class="list-group-item toLearnSkills">APIs</li>
+                    <li class="list-group-item toLearnSkills">Node.js</li>
+                    <li class="list-group-item toLearnSkills">NPM</li>
+                    <li class="list-group-item toLearnSkills">JSON  <i class="bi bi-filetype-json"></i></li>
+                    <li class="list-group-item toLearnSkills">ECMAScript6</li>
+                    <li class="list-group-item toLearnSkills">Object-Oriented Programing</li>
+                    <li class="list-group-item toLearnSkills">Test-Driven Development</li>
+                    <li class="list-group-item toLearnSkills">React.js</li>
+                    <li class="list-group-item toLearnSkills">React Hooks</li>
+                    <li class="list-group-item toLearnSkills">JSX</li>
+                </ul>
+            </div>
+        </section> 
+
+
     </>
     )
 }
--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,8 +1,17 @@
+import AndreaPhoto from "../../assets/images/AndreaPhoto.jpeg"
+
 const Home = () => {
 
     return (
     <>
         <h1>Welcome to my portfolio!</h1>
+        <p>Hello, I'm Andrea Melisa and I am a Front End Developer!</p>
+
+        <div className="photo">
+			<img src={AndreaPhoto} alt="Andrea Martinez smiling" />
+		</div>
+
+        <p>I try to be a very detail oriented person! </p>
     </>
     )
 }
